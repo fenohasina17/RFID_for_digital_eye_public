fbab6fff793783ec1db2004a46c6a0c8ddd78fa9
fenohasina <fenohasinarasoavarison@gmail.com>
2021-09-11 14:33:12 +0300

    project initialisation

---
+++ src/test/java/rfid_for_digital_eye/RfidForDigitalEyeApplicationTests.java
@@ -0,0 +1,13 @@
+package rfid_for_digital_eye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfidForDigitalEyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
